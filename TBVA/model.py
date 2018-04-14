3edc36f1ff62c6846f9f4bf9e500b651bb6bd004
ZaherAirout <m.zaher.airout@gmail.com>
2018-04-14 10:21:50 +0300

    update model

--- TBVA/model.py
+++ TBVA/model.py
@@ -1,44 +1,45 @@
-import numpy
 import torch.nn as nn
-import torch.nn.functional as F
-from torch.nn import Linear
 from torchvision import models
+
+from TBVA.multi_modal_layer import MultiModalLayer
 
 
 class m_RNN(nn.Module):
     def __init__(self,
                  answer_length,
-                 vocab_count,
                  embed2_features,
                  rnn_features,
                  attention_features,
                  cnn_features,
                  L, D,
+                 vocab_count=9956,
                  multimodal_features=2048,
                  ):
         super().__init__()
         self.answer_length = answer_length
 
+        embeds_1_size = 1024
+        embeds_2_size = 2048
+        rnn_size = 2048
+        attention_size = 1024
+        cnn_features_size = 1024
+        multimodal_in_size = 2048
+        multimodal_out_size = 1024
         vgg16 = models.vgg16(pretrained=True)
         modules = list(vgg16.children())[:-1]  # delete the last fc layer.
         self.feature_extractor = nn.Sequential(*modules)
-        self.attention_layer = nn.Linear(vgg16.fc.in_features, 1)
+        self.attention_layer = nn.Linear(vgg16.fc.in_features, attention_size)
 
-        self.embeds_1 = nn.Embedding(answer_length, 1024)
-        self.embeds_2 = nn.Embedding(2, 2048)
-        self.softmax = F.softmax(1024, vocab_count)
+        self.embeds_1 = nn.Embedding(answer_length, embeds_1_size)
+        self.embeds_2 = nn.Embedding(embeds_1_size, embeds_2_size)
 
-        # MultiModal Layers
-        self.linear_embed2 = Linear(embed2_features, multimodal_features)
-        self.linear_rnn = Linear(rnn_features, multimodal_features)
-        self.linear_attention = Linear(attention_features, multimodal_features)
-        self.linear_cnn = Linear(cnn_features, multimodal_features)
+        rnn_layers = 1
+        self.rnn_cell = nn.LSTMCell(embeds_2_size, rnn_size, rnn_layers)
 
-        self.intermediate = nn.Linear(multimodal_features, 1024)
+        self.multi_modal = MultiModalLayer(embeds_2_size, rnn_size, attention_size, cnn_features_size,
+                                           multimodal_in_size, multimodal_out_size)
 
-    @staticmethod
-    def g(x):
-        return 1.7159 * numpy.tanh((2 / 3) * x)
+        # self.softmax = F.softmax(self.intermediate, vocab_count)
 
     def forward(self, image):
         return None
@@ -46,4 +47,4 @@ class m_RNN(nn.Module):
 
 if __name__ == '__main__':
     print('M_RNN')
-    # m_RNN()
+    x = m_RNN()
---
+++ TBVA/multi_modal_layer.py
@@ -0,0 +1,30 @@
+import torch.nn.functional as F
+from torch.nn import Module, Linear
+
+
+class MultiModalLayer(Module):
+    def __init__(self,
+                 embed2_features,
+                 rnn_features,
+                 attention_features,
+                 cnn_features,
+                 multimodal_features,
+                 out_features):
+        super().__init__()
+
+        # MultiModal Layers
+
+        self.linear_embed2 = Linear(embed2_features, multimodal_features)
+        self.linear_rnn = Linear(rnn_features, multimodal_features)
+        self.linear_attention = Linear(attention_features, multimodal_features)
+        self.linear_cnn = Linear(cnn_features, multimodal_features)
+        self.intermediate = Linear(multimodal_features, out_features)
+
+    def forward(self, embed2, rnn, attention, cnn):
+        w = self.linear_embed2(embed2)
+        r = self.linear_rnn(rnn)
+        v = self.linear_cnn(cnn)
+        z = self.linear_attention(attention)
+        intermediate = self.intermediate(w + r + v + z)
+
+        return 1.7159 * F.tanh((2 / 3) * intermediate)
